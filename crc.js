3afbc5341f6d58daa89e56a6652e50d4312e8941
Ben Brook <bencmbrook@users.noreply.github.com>
2019-07-10 10:55:47 -0700

    (just) linting (#23)
    * Add linting
    
    * More linting
    
    * allow plusplus and require import extensions

--- crc.js
+++ crc.js
@@ -1,32 +1,34 @@
 class Crc32 {
-  constructor () {
-    this.crc = -1
+  constructor() {
+    this.crc = -1;
   }
 
-  append (data) {
-    var crc = this.crc | 0; var table = this.table
-    for (var offset = 0, len = data.length | 0; offset < len; offset++) {
-      crc = (crc >>> 8) ^ table[(crc ^ data[offset]) & 0xFF]
+  append(data) {
+    let crc = this.crc | 0;
+    const { table } = this;
+    for (let offset = 0, len = data.length | 0; offset < len; offset++) {
+      crc = (crc >>> 8) ^ table[(crc ^ data[offset]) & 0xFF];
     }
-    this.crc = crc
+    this.crc = crc;
   }
 
-  get () {
-    return ~this.crc
+  get() {
+    return ~this.crc;
   }
 }
+
 Crc32.prototype.table = (() => {
-  var i; var j; var t; var table = []
+  let i; let j; let t; const table = [];
   for (i = 0; i < 256; i++) {
-    t = i
+    t = i;
     for (j = 0; j < 8; j++) {
       t = (t & 1)
         ? (t >>> 1) ^ 0xEDB88320
-        : t >>> 1
+        : t >>> 1;
     }
-    table[i] = t
+    table[i] = t;
   }
-  return table
-})()
+  return table;
+})();
 
-export default Crc32
+export default Crc32;
