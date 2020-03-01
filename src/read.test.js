8d215beca1f4226457348b5d94ff45a9bec129c0
Ben Brook <benbrook@me.com>
2020-03-01 15:10:26 -0800

    Tests + coverage working

---
+++ src/read.test.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('autopass read', (t) => {
+  t.pass('asdf');
+  t.end();
+});
---
+++ src/write.test.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('autopass write', (t) => {
+  t.pass('asdf');
+  t.end();
+});
