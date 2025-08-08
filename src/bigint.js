f034ffe0dc4582b39adc2170c46946049fc5e74c
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-08 10:49:09 -0400

    allow library consumer to bring-your-own JSBI polyfill

--- src/bigint.js
+++ src/bigint.js
@@ -1,54 +1,60 @@
 /* global globalThis BigInt */
 
 /**
- * If BigInt is natively supported, library consumer should polyfill JSBI
- * @see https://github.com/GoogleChromeLabs/jsbi/blob/master/jsbi.d.ts
- * @see https://github.com/GoogleChromeLabs/babel-plugin-transform-jsbi-to-bigint/blob/master/src/index.js
+ * Use JSBI syntax for BigInt operations, instead of calling BigInt directly.
+ *
+ * This is NOT a polyfill. This uses native BigInt. Using this syntax simply makes it possible to polyfill BigInt.
+ * If BigInt is not natively supported (ES2020+), library consumer MUST expose globalThis.JSBI before using Conflux.
+ *
+ * @see https://github.com/GoogleChromeLabs/jsbi - JSBI library for why BigInt
+ * @see https://github.com/GoogleChromeLabs/jsbi/blob/master/jsbi.d.ts - types
  */
 if (!globalThis.BigInt) {
   throw new Error('BigInt is not supported in this browser');
 }
 
-const jsbi = {};
+const jsbi = globalThis.JSBI || {};
 
-// constructor
-jsbi.BigInt = (a) => BigInt(a);
+if (!jsbi.BigInt) {
+  // constructor
+  jsbi.BigInt = (a) => BigInt(a);
 
-// note: JSBI toString is already the same: a.toString()
-jsbi.toNumber = (a) => Number(a);
+  // note: JSBI toString is already the same: a.toString()
+  jsbi.toNumber = (a) => Number(a);
 
-// binary functions to expressions
-jsbi.add = (a, b) => a + b;
-jsbi.subtract = (a, b) => a - b;
-jsbi.multiply = (a, b) => a * b;
-jsbi.divide = (a, b) => a / b;
-jsbi.remainder = (a, b) => a % b;
-jsbi.exponentiate = (a, b) => a ** b;
-jsbi.leftShift = (a, b) => a << b;
-jsbi.signedRightShift = (a, b) => a >> b;
-jsbi.bitwiseAnd = (a, b) => a & b;
-jsbi.bitwiseOr = (a, b) => a | b;
-jsbi.bitwiseXor = (a, b) => a ^ b;
-jsbi.equal = (a, b) => a === b;
-jsbi.notEqual = (a, b) => a !== b;
-jsbi.lessThan = (a, b) => a < b;
-jsbi.lessThanOrEqual = (a, b) => a <= b;
-jsbi.greaterThan = (a, b) => a > b;
-jsbi.greaterThanOrEqual = (a, b) => a >= b;
-jsbi.EQ = (a, b) => a === b;
-jsbi.NE = (a, b) => a !== b;
-jsbi.LT = (a, b) => a < b;
-jsbi.LE = (a, b) => a <= b;
-jsbi.GT = (a, b) => a > b;
-jsbi.GE = (a, b) => a >= b;
-jsbi.ADD = (a, b) => a + b;
+  // binary functions to expressions
+  jsbi.add = (a, b) => a + b;
+  jsbi.subtract = (a, b) => a - b;
+  jsbi.multiply = (a, b) => a * b;
+  jsbi.divide = (a, b) => a / b;
+  jsbi.remainder = (a, b) => a % b;
+  jsbi.exponentiate = (a, b) => a ** b;
+  jsbi.leftShift = (a, b) => a << b;
+  jsbi.signedRightShift = (a, b) => a >> b;
+  jsbi.bitwiseAnd = (a, b) => a & b;
+  jsbi.bitwiseOr = (a, b) => a | b;
+  jsbi.bitwiseXor = (a, b) => a ^ b;
+  jsbi.equal = (a, b) => a === b;
+  jsbi.notEqual = (a, b) => a !== b;
+  jsbi.lessThan = (a, b) => a < b;
+  jsbi.lessThanOrEqual = (a, b) => a <= b;
+  jsbi.greaterThan = (a, b) => a > b;
+  jsbi.greaterThanOrEqual = (a, b) => a >= b;
+  jsbi.EQ = (a, b) => a === b;
+  jsbi.NE = (a, b) => a !== b;
+  jsbi.LT = (a, b) => a < b;
+  jsbi.LE = (a, b) => a <= b;
+  jsbi.GT = (a, b) => a > b;
+  jsbi.GE = (a, b) => a >= b;
+  jsbi.ADD = (a, b) => a + b;
 
-// unary functions to expressions
-jsbi.unaryMinus = (a) => -a;
-jsbi.bitwiseNot = (a) => ~a;
+  // unary functions to expressions
+  jsbi.unaryMinus = (a) => -a;
+  jsbi.bitwiseNot = (a) => ~a;
 
-// static methods
-jsbi.asIntN = (a, b) => BigInt.asIntN(a, b);
-jsbi.asUintN = (a, b) => BigInt.asUintN(a, b);
+  // static methods
+  jsbi.asIntN = (a, b) => BigInt.asIntN(a, b);
+  jsbi.asUintN = (a, b) => BigInt.asUintN(a, b);
+}
 
 export default jsbi;
