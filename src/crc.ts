a1e345b88a1d666800b738cd5f0d9af6435b2603
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-12 16:56:30 -0400

    clean up crc32

--- src/crc.ts
+++ src/crc.ts
@@ -1,22 +1,32 @@
+// Generate CRC32 lookup table
+function generateCrcTable(): number[] {
+  const table: number[] = [];
+  for (let index = 0; index < 256; index++) {
+    let t = index;
+    for (let bitIndex = 0; bitIndex < 8; bitIndex++) {
+      t = t & 1 ? (t >>> 1) ^ 0xed_b8_83_20 : t >>> 1;
+    }
+    table[index] = t;
+  }
+  return table;
+}
+
 class Crc32 {
-  crc: number;
+  private crc: number;
+  private static readonly table: number[] = generateCrcTable();
 
   constructor() {
     this.crc = -1;
   }
 
-  append(data: Uint8Array): void {
-    // eslint-disable-next-line unicorn/prefer-math-trunc
-    let crc = this.crc | 0;
-    // @ts-expect-error - table is not defined on the instance
-    const { table } = this;
-    // eslint-disable-next-line unicorn/prefer-math-trunc
-    for (let offset = 0, length = data.length | 0; offset < length; offset++) {
+  public append(data: Uint8Array): void {
+    let crc = Math.trunc(this.crc);
+    const { table } = Crc32;
+    for (let offset = 0, length = Math.trunc(data.length); offset < length; offset++) {
       const byte = data[offset];
       if (byte === undefined) {
         continue;
       }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       const tableEntry = table[(crc ^ byte) & 0xff];
       if (tableEntry === undefined) {
         continue;
@@ -26,27 +36,9 @@ class Crc32 {
     this.crc = crc;
   }
 
-  get(): number {
+  public get(): number {
     return (this.crc ^ -1) >>> 0;
   }
 }
-
-Crc32.prototype.table = ((
-  table: number[],
-  // eslint-disable-next-line unicorn/prevent-abbreviations
-  i: number,
-  // eslint-disable-next-line unicorn/prevent-abbreviations
-  j: number,
-  t: number,
-) => {
-  for (i = 0; i < 256; i++) {
-    t = i;
-    for (j = 0; j < 8; j++) {
-      t = t & 1 ? (t >>> 1) ^ 0xed_b8_83_20 : t >>> 1;
-    }
-    table[i] = t;
-  }
-  return table;
-})([], 0, 0, 0);
 
 export default Crc32;
